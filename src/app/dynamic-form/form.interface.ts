8276c312c61823504526c53de9ef97b63057a48a
Abirami <abirami.muthukumarasamy@impelox.com>
2024-09-20 17:51:45 +0530

    Interface Added

--- src/app/dynamic-form/form.interface.ts
+++ src/app/dynamic-form/form.interface.ts
@@ -1,4 +1,4 @@
-export interface IFormStructure {
+export interface ISingleInputStructure {
     type: string;
     label: string;
     name: string;
@@ -9,4 +9,46 @@ export interface IFormStructure {
       validator: string;
       message: string;
     }[];
-  }
+}
+
+export interface IFormStructure {
+  id: number
+  header: string
+  description: string
+  type: string //table or form or widget
+  form_fields: ISingleInputStructure
+}
+  
+
+export interface RuleGroupTableStructure {
+  uw_rule_group_id: number
+  rule_group_name: string
+  show_ui: number
+  apply_rule: number
+  created_by:number
+  created_date: string
+  modified_date:string
+}
+// export interface IConditionStructure {
+//   field1: string
+//   operator: string
+//   field2 :string
+// }
+// export interface IEvaluationStructure {
+//   condition_structure :IConditionStructure[]
+// }
+export interface UWRuleTableStructure {
+  uw_rule_id: number
+  uw_rule_group_id :number
+  form_config: IFormStructure
+  form_name:string
+  evaluation: any //SHOULD CHANGE
+  show_ui: number
+  apply_rule: number
+  outcome: string
+  loading_premium: string
+  factor: string
+  created_by: string|number
+  created_date: string
+  modified_date:string
+}
