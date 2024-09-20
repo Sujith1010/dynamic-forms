ac56d7fd4bb77a74372b6213f823d1d2987f9fe7
Abirami <abirami.muthukumarasamy@impelox.com>
2024-09-20 19:01:25 +0530

    Changes

---
+++ src/app/api.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from "@angular/core";
+import axios, { AxiosInstance } from 'axios';
+
+
+@Injectable({
+  providedIn: "root",
+})
+export class ApiService {
+  revisedPremium:any;
+  secretKeyConfig={ };
+  tokenFromUI = "93389a26699r89k3";
+  encrypted:any;
+    private apiUrl = 'http://localhost:3000';
+  private axios: AxiosInstance;
+  constructor() {
+    this.axios = axios.create({
+      baseURL:this.apiUrl,
+    });
+  }
+
+
+  async getApiCall(url: string, params: any): Promise<any> {
+    try {
+      let response = await this.axios.get(url);
+      return response["data"];
+    } catch (exception:any) {
+            throw exception;
+    }
+  }
+
+  async postApiCall(url: string, params: any): Promise<any> {
+    try {
+      let response = await this.axios.post(url, params);
+      return response['data'];
+    } catch (exception: any) {
+      throw exception;
+    }
+  }
+
+  async putApiCall(url: string, params: any): Promise<any> {
+    try {
+      let response = await this.axios.put(url, params);
+      return response['data'];
+    } catch (exception: any) {
+      throw exception;
+    }
+  }
+
+  async deleteApiCall(url: string): Promise<any> {
+    try {
+      let response = await this.axios.delete(url);
+      return response['data'];
+    } catch (exception: any) {
+      throw exception;
+    }
+  }
+
+}
--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RuleGroupTableStructure } from './form.interface';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
   styleUrls: ['./dynamic-form.component.css']
 })
-export class DynamicFormComponent {
+export class DynamicFormComponent implements OnInit {
+  ruleGroups:RuleGroupTableStructure[] = []
+  constructor() { }
+  async ngOnInit() {
+    
+  }
 
 }
